dcdab24aef72d2cfffbd23a07915103ecf362ea7
Fabio <fabiobenoit@hotmail.fr>
2017-07-23 20:01:46 +0200

    French + weather forecast

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+var audio = new Audio('activate.wav');
+audio.volume = 0.5;
+var synth = window.speechSynthesis;
+
 function startRecognition(){
 	recognition = new webkitSpeechRecognition();
 	recognition.continuous = false;
 	recognition.interimResults = false;
+
 
 	var synth = window.speechSynthesis;
 
@@ -21,10 +26,56 @@ function startRecognition(){
 		for(var i = event.resultIndex; i < event.results.length; i++){
 			text += event.results[i][0].transcript;
 		}
+		console.log(text);
+		if(text.toLowerCase() == "écho"){
+			echo();
+		}
+		else{
+			startRecognition();
+		}
+	};
+
+	function echo() {
+		recognition.stop();
+		audio.play();
+		setTimeout(startRecognitionSent(), 1000)
+	}
+
+	recognition.lang = "fr-FR";
+	recognition.start();
+
+
+
+	
+}
+
+function startRecognitionSent() {
+	recognition = new webkitSpeechRecognition();
+	recognition.continuous = false;
+	recognition.interimResults = false;
+
+	
+
+	recognition.onstart = function(event) {
+		console.log("c'est vraiment parti")
+	};
+
+	recognition.onaudioend = function(){
+		console.log("pd");
+		startRecognition();
+	}
+
+	recognition.onresult = function(event) {
+		recognition.onaudioend = null;
+
+		var text = "";
+		for(var i = event.resultIndex; i < event.results.length; i++){
+			text += event.results[i][0].transcript;
+		}
 		send(text);
 	};
 
-	recognition.lang = "en-US";
+	recognition.lang = "fr-FR";
 	recognition.start();
 
 	function send(text){
@@ -35,17 +86,22 @@ function startRecognition(){
 			contentType: "application/json; charset=utf-8",
 			dataType: "json",
 			headers: {
-				"Authorization": "Bearer " + "dce399808780466db898fad9bfae71fe"
+				"Authorization": "Bearer " + "37af3641619a4f14877a0480c1b61388"
 			},
-			data: JSON.stringify({query: text, lang: "en", sessionId: "yaydevdiner"}),
+			data: JSON.stringify({query: text, lang: "fr", sessionId: "PaaBliiTo"}),
 
 			success: function(data){
 				console.log(data.result.speech);
 				recognition.stop();
 				var utterance = new SpeechSynthesisUtterance(data.result.speech);
-				utterance.lang = 'en-US';
+				utterance.lang = 'fr-FR';
 				utterance.onend = function() {
-					startRecognition();
+					if(data.result.action == "weather"){
+						meteo(data);
+					}
+					else{
+						startRecognition();	
+					}
 				};
 				synth.speak(utterance);
 			},
@@ -54,4 +110,36 @@ function startRecognition(){
 			}
 		});
 	}
+}
+
+function meteo(data){
+	var city;
+	if(typeof data.result.parameters.address === "object" || data.result.parameters.address instanceof Object){
+		city = data.result.parameters.address.city;
+	}
+	else{
+		city = data.result.parameters.address;
+	}
+	console.log(city);
+	$.ajax({
+		type: "GET",
+		url: "https://api.apixu.com/v1/forecast.json?key=412c2e68396a4a73835130643170407&q="+city+"&lang=fr",
+		contentType: "aaplication/json; charset=utf-8",
+		dataType: "json",
+		success: function(data){
+			var sentence = "Les prévisions pour "+data.location.name+" aujourd'hui sont : "+data.forecast.forecastday[0].day.condition.text+" avec \
+			une température maximale de "+Math.round(data.forecast.forecastday[0].day.maxtemp_c)+" degrés et une minimale \
+			de "+Math.round(data.forecast.forecastday[0].day.mintemp_c)+" degrés";
+			console.log(sentence);
+			var utterance = new SpeechSynthesisUtterance(sentence);
+			utterance.lang = 'fr-FR';
+			utterance.onend = function(){
+				startRecognition();
+			};
+			synth.speak(utterance);
+		},
+		error: function(){
+			console.log("error");
+		}
+	});
 }
